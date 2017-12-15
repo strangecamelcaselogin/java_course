5e414c9e09980f14fc1e365068ef7972173593b7
strangecamelcaselogin <strangecamelcaselogin@gmail.com>
2017-12-15 22:18:51 +0300

    factoryAndAnnotations: кое-какие правки

--- 4_0/factoryAndAnnotations/src/main/java/ru/rsatu/core/UselessFactory.java
+++ 4_0/factoryAndAnnotations/src/main/java/ru/rsatu/core/UselessFactory.java
@@ -1,15 +1,25 @@
 package ru.rsatu.core;
 
+import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 
 public class UselessFactory {
     public static TestInterface getTest() throws ClassNotFoundException{
+        Test test = new Test();
+
+        boolean isNeedToProxy = false;
+        for (Method m: test.getClass().getMethods()) {
+            if (m.isAnnotationPresent(Log.class)) {
+                isNeedToProxy = true;
+                break;
+            }
+        }
 
         // Создадим прокси объект и вернем его
-        return (TestInterface) Proxy.newProxyInstance(
-                Test.class.getClassLoader(),
-                Test.class.getInterfaces(),
-                new LogInvocation(new Test(), Log.class));
+        return isNeedToProxy ? (TestInterface) Proxy.newProxyInstance(
+                test.getClass().getClassLoader(),
+                test.getClass().getInterfaces(),
+                new LogInvocation(test, Log.class)) : test;
     }
 
 }
