6c016d8e5b0c5a719c20875d15e8c999bedbd151
strangecamelcaselogin <strangecamelcaselogin@gmail.com>
2018-01-27 15:17:08 +0300

    обновленный jaxB, новая xsd, новый jaxb2 плагин

--- 4_1/jaxbP/src/main/java/ru/rsatu/App.java
+++ 4_1/jaxbP/src/main/java/ru/rsatu/App.java
@@ -12,6 +12,7 @@ import javax.xml.validation.SchemaFactory;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.net.URL;
 import java.util.List;
 
 
@@ -21,32 +22,36 @@ public class App {
 
         try {
             SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
-            Schema schema = schemaFactory.newSchema(new File("./university.xsd"));
+
+            URL xsd = App.class.getClassLoader().getResource("university.xsd");
+            URL xml = App.class.getClassLoader().getResource("university.xml");
+
+            Schema schema = schemaFactory.newSchema(xsd);
 
             JAXBContext jaxbContext = JAXBContext.newInstance(University.class);
 
             //получить из xml объект
             Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
             unmarshaller.setSchema(schema);
-            University university = (University) unmarshaller.unmarshal(new FileReader("./university.xml"));
+            University university = (University) unmarshaller.unmarshal(xml);
             Faculty faculty = university.getFaculty();
             university.setFaculty(faculty);
             Departments departments = faculty.getDepartments();
-            List<Department> departmentList = departments.getDepartment();
+            List<Department> departmentList = departments.getDepartments();
 
             //кусок бесполезного кода - на всякий случай
             for (Department d: departmentList) {
                 Groups groups = d.getGroups();
-                List<Group> groupList = groups.getGroup();
+                List<Group> groupList = groups.getGroups();
                 for (Group g: groupList){
                     if (g.getGroupName().equals("ПИМ-17")){
                         g.setGroupName("ПИМ-2017");
                     }
                     Students students = g.students;
-                    List<Student> studentList = students.getStudent();
+                    List<Student> studentList = students.getStudents();
                     for (Student s: studentList){
                         Subjects subjects = s.getSubjects();
-                        List<Subject> subjectList = subjects.getSubject();
+                        List<Subject> subjectList = subjects.getSubjects();
                         for (Subject sub: subjectList){
                             String name = sub.getSubjectName();
                             String mark = sub.getMark();
@@ -65,8 +70,6 @@ public class App {
             jaxbMarshaller.marshal(university, System.out);
 
         } catch (JAXBException e) {
-            e.printStackTrace();
-        } catch (FileNotFoundException e){
             e.printStackTrace();
         }
 
